8136ccac5622f440384cf62018c12468c5c65131
KamranG12 <kamran.Jabiyev@aplusa-security.com>
2017-05-14 11:02:24 +0400

    first

---
+++ 12.05.2017/Cars.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._05._2017
+{
+    class Cars
+    {
+        private double localDistance;
+        private double globalDistance;
+        private double fuelUsage;
+        private int fuelCapacity=100;
+        private double fuelCurrent=20;
+
+        public double LocalDistance
+        {
+            set
+            {
+                this.localDistance = value;
+            }
+            get
+            {
+                return this.localDistance;
+            }
+        }
+        
+        public double GlobalDistance
+        {
+            get
+            {
+                return this.globalDistance;
+            }
+        }
+
+        public double FuelUsage
+        {
+            set
+            {
+                this.fuelUsage = value;
+            }
+            get
+            {
+                return this.fuelUsage;
+            }
+        }
+
+        public int FuelCapacity
+        {
+            get
+            {
+                return this.fuelCapacity;
+            }
+        }
+
+
+        /// <summary>
+        ///  Yoxlayirki bakda olan benzin-e elave olundugu halda bak movcudlugunda cox olur mu olmayirmi
+        /// </summary>
+        /// <param name="litr">Ne qeder benzin elave edilmek istendiyi gonedrilecek</param>
+        /// <returns></returns>
+        public bool checkFuelAviable(double litr)
+        {
+            if (this.fuelCurrent + litr <= this.FuelCapacity)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Baka benzin elave edir
+        /// </summary>
+        /// <param name="litr">Elave edilecek benzin miqdari</param>
+        /// <returns></returns>
+        public void addFuel(double litr)
+        {
+                this.fuelCurrent += litr; 
+        }
+        /// <summary>
+        /// Global distance artirir
+        /// </summary>
+        /// <param name="km"></param>
+        public void ascGlobalDistance(double km)
+        {
+            this.globalDistance += km;
+        }
+        /// <summary>
+        /// Local distance atirir
+        /// </summary>
+        /// <param name="km"></param>
+        public void ascLocalDistance(double km)
+        {
+           this.LocalDistance += km;
+        }
+        /// <summary>
+        /// Movcud benzin-le nece km yol gedile bileceyini qayatrir
+        /// </summary>
+        /// <returns></returns>
+        public double possibleDistance()
+        {
+            return (this.fuelCurrent*100)/this.FuelUsage;
+        }
+        /// <summary>
+        ///  Girilen km gedib gede bilmeyeyeceni tapan funksiya 
+        /// </summary>
+        /// <param name="km"></param>
+        /// <returns></returns>
+        public bool checkDistance(double km)
+        {
+            if (this.possibleDistance() >= km)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ 12.05.2017/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _12._05._2017;
+
+namespace _12._05._2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car_option=new Cars();
+            car_option.FuelUsage=10;
+            car_option.possibleDistance();
+            Console.Write("Neche km gedeceyinizi daxil edin:");
+            double km=Convert.ToDouble(Console.ReadLine());
+            if(car_option.checkDistance(km))
+            {
+              Console.WriteLine("Bu yolu getmesi uchun benzin yeterlidir");
+                car_option.ascGlobalDistance(km);
+                car_option.ascLocalDistance(km);
+            }else
+            {
+              Console.WriteLine("Benzin yeterli deyil");
+            }
+            Console.Write("Neche litr bezin dolduracaginizi daxil edin:");
+            double litr=Convert.ToDouble(Console.ReadLine());
+            if(car_option.checkFuelAviable(litr))
+            {
+              Console.WriteLine("benzin yanacaq chenine elave olundu");
+                car_option.addFuel(litr);
+            }else
+            {
+              Console.WriteLine("Yanacaq cheninin tutumunu ashirsiniz");
+            }
+            Console.ReadKey();
+            /*Console.WriteLine("1. Masini sur (paramter olaraq km olacaq sistem yoxlamasi gerekirki bak-da olan benzin-le bu qeder gede biler mi?)");
+            Console.WriteLine("2. Benzin doldur (parametr olaraq litr alacaq. Yoxlayacaqki elave olunan litr bak-in kapasitesini asirmi? )");
+            Console.WriteLine("3. Local Distance. Masinin local olaraq ne qeder yol getdiyini deyecek ve sifirlana bilecek");
+            Console.WriteLine("4. Global Distance. Masinin global olaraq ne qeder getdiyini deyecek ve sifirlana bilinmeyecek");
+            Console.WriteLine("0. Exit");*/
+        }
+    }
+}
